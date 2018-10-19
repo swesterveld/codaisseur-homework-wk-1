07e31cfc317c189999f7850171ac1cb06026482a
Sil Westerveld <git@silwesterveld.com>
2018-10-19 18:11:21 +0200

    Implement hero object to let its tests pass

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,12 @@
 // Write your JS here
+
+// Hero Object
+let hero = {};
+hero.name = '';
+hero.heroic = new Boolean();
+hero.inventory = [];
+hero.health = 10;
+hero.weapon = {
+  type: '',
+  damage: 2
+};
